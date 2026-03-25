07efb5209ad7936e63dd72d6620d2206c4158416
mrmerarif <bflores2@byupathway.edu>
2026-03-25 00:23:31 -0600

    fixing the validation

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,5 +1,2 @@
-// Current Year
 document.getElementById("currentyear").textContent = new Date().getFullYear();
-
-// Last Modified
 document.getElementById("lastModified").textContent = document.lastModified;
